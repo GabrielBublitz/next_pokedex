0f329fcfe6ab07d2b61ac60f0d1ea4c467b4eba6
Gabriel Bublitz <gabriel.bublitz@hotmail.com>
2023-05-30 22:13:37 -0300

    Add new component Card

---
+++ src/components/card.js
@@ -0,0 +1,21 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import styles from '../app/page.module.css'
+import Link from 'next/link'
+
+export default function Card({ href, title, text }) {
+    const router = useRouter()
+    const handleClick = (e) => {
+        e.preventDefault()
+        router.push(href)
+    }
+
+    return (
+        <Link href={href} onClick={handleClick} className={styles.card} rel="noopener noreferrer">
+            <h2>
+                {title} <span>-&gt;</span>
+            </h2>
+            <p>{text}</p>
+        </Link>
+    )
+}
