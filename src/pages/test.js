c222793860b241bbf5f4c13437e89a0e5d240cca
Gabriel Bublitz <gabriel.bublitz@hotmail.com>
2023-05-31 22:41:06 -0300

    Add list of first 16 pokémons when load screen

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,17 +1,40 @@
 import { useState, useEffect } from 'react'
 import axios from "axios"
+import Head from 'next/head'
 
 export default () => {
+    const baseUrl = 'https://pokeapi.co/api/v2/'
 
-    const [pokeName, setPokeName] = useState('ditto')
     const [poke, setPoke] = useState()
+    const [pokeName, setPokeName] = useState('ditto')
+    const [pokemons, setPokemons] = useState([])
+
+    const style = {
+        display: 'inline-block'
+    }
 
     useEffect(() => {
+        test()
         GetPoke()
     }, [])
 
+    async function test() {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=16&offset=0')
+        const data = await response.data
+
+        var pokes = []
+
+        for (const item of data.results) {
+            const response = await axios.get(item.url);
+            const result = response.data;
+            pokes.push(result);
+        }
+
+        setPokemons(pokes)
+    }
+
     function GetPoke() {
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+        axios.get(`${baseUrl}pokemon/${pokeName}`)
             .then((response) => {
                 return response.data
             })
@@ -23,18 +46,29 @@ export default () => {
             })
     }
 
-    function CallApi(){
-        GetPoke(pokeName)
+    function CallApi() {
+        GetPoke()
     }
 
-    return <>
+    return (
         <div>
-            <input type='text' onChange={(e) => setPokeName(e.target.value.toLowerCase().trim().replace(' ', '-'))}/>
+            <Head>
+                <title>Pokédex</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+
+            <input type='text' onChange={(e) => setPokeName(e.target.value.toLowerCase().trim().replace(' ', '-'))} />
             <button onClick={CallApi}>Search</button>
-            <h1>
-                {poke ? (`${poke.name.replace('-', ' ')} #${poke.id}`) : (`No data`)}
-            </h1>
-            <img src={poke ? (poke.sprites.other['official-artwork'].front_default) : ''} />
+
+            {pokemons.length > 0 ? (pokemons.map((pokemon, index) => {
+                return (
+                    <div style={style} key={pokemon.id}>
+                        <h1>
+                            {pokemon ? (`${pokemon.name} #${pokemon.id}`) : (`No data`)}
+                        </h1>
+                        <img src={pokemon ? (pokemon.sprites.other['official-artwork'].front_default) : ''} />
+                    </div>)
+            })) : (<div>No data</div>)}
         </div>
-    </>
+    )
 }
