50730800b36f46261553d246e4642ef90d86ca65
Gabriel Henrique Mota Bublitz <gabriel.bublitz@hotmail.com>
2024-12-24 23:50:10 -0300

    Fix

--- src/pages/pokedex.js
+++ src/pages/pokedex.js
@@ -79,7 +79,7 @@ export default function Pokedex() {
                 <title>Pokédex</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" async></script>
             </Head>
             <section style={{ display: 'flex', justifyContent:'center', padding: '30px 0px' }}>
                 <SearchBar onSearch={handleSearch} />
